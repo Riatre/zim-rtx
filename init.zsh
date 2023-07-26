e34158374afaa23802f6ea4c3cd1cad637e30435
Riatre Foo <foo@riat.re>
2023-07-27 05:08:03 +0800

    Do not run hook-env during startup
    It will be run in precmd anyways.
    
    Also, quote everything.

--- init.zsh
+++ init.zsh
@@ -1,19 +1,18 @@
 (( ${+commands[rtx]} )) && () {
-  local command=${commands[rtx]}
+  local command="${commands[rtx]}"
 
   # generating activation file
-  local activatefile=$1/rtx-activate.zsh
-  if [[ ! -e $activatefile || $activatefile -ot $command ]]; then
-    $command activate zsh >| $activatefile
-    zcompile -UR $activatefile
+  local activatefile="$1/rtx-activate.zsh"
+  if [[ ! -e "$activatefile" || "$activatefile" -ot "$command" ]]; then
+    "$command" activate zsh >| "$activatefile"
+    zcompile -UR "$activatefile"
   fi
 
-  source $activatefile
-  source <($command hook-env -s zsh)
+  source "$activatefile"
 
   # generating completions
-  local compfile=$1/functions/_rtx
-  if [[ ! -e $compfile || $compfile -ot $command ]]; then
-    $command complete --shell zsh >| $compfile
+  local compfile="$1/functions/_rtx"
+  if [[ ! -e "$compfile" || "$compfile" -ot "$command" ]]; then
+    "$command" complete --shell zsh >| "$compfile"
   fi
 } ${0:h}
