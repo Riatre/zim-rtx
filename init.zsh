ce680b18344af0b582245eb446f63e806d29d0cb
Riatre Foo <foo@riat.re>
2023-07-27 05:06:47 +0800

    Shhhhh...

--- init.zsh
+++ init.zsh
@@ -15,6 +15,5 @@
   local compfile=$1/functions/_rtx
   if [[ ! -e $compfile || $compfile -ot $command ]]; then
     $command complete --shell zsh >| $compfile
-    print -u2 -PR "* Detected a new version 'rtx'. Regenerated completions."
   fi
 } ${0:h}
